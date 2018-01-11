005c04f81dab4893ffac48e8c6a602d3d0939dae
Noam Yefet <snyefet@csf.technion.ac.il>
2018-01-11 14:24:03 +0200

    bug fix

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -1,11 +1,26 @@
 import socket
 import select
 
-openConnections = {}
+
+
 
 serversIP = ["192.168.0.101", "192.168.0.102", "192.168.0.103"]
 
 serversSockets = []
+clientsOpenedSockets = []
+
+requestsQueue = []
+
+isServerAvailable = {}
+handledConnections = {}
+
+
+def peekServer(serversList):
+    for s in serversList:
+        if isServerAvailable[s]:
+            return s
+    return None
+
 
 # connectiong to servers
 for ip in serversIP:
@@ -14,49 +29,85 @@ for ip in serversIP:
     print('connecting to %s port %s' % server_address)
     sock.connect(server_address)
     serversSockets.append(sock)
+    isServerAvailable[sock] = True
 
 # wait for clients
 # Create a TCP/IP socket
-clientsSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+listeningSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 server_address = ('10.0.0.1', 80)
 print ('starting up on %s port %s' % server_address)
-clientsSock.bind(server_address)
-
+listeningSock.bind(server_address)
 # Listen for incoming connections
-clientsSock.listen(1)
+listeningSock.listen(1)
 
 while True:
     # Wait for a connection
-    print ( 'waiting for a connection')
-    connectionToClient, client_address = clientsSock.accept()
+    # print ( 'waiting for a connection')
+    # connectionToClient, client_address = clientsSock.accept()
 
     # print('connection from', client_address)
-    data = connectionToClient.recv(2)
-    print('received "%s"' % data)
-    readable, writable, _ = select.select(serversSockets, serversSockets, [])
-    print("writeable: ", writable)
-    print("readable: ", readable)
+
+    # print('received "%s"' % data)
+    readable, writable, _ = select.select(serversSockets + clientsOpenedSockets + [listeningSock], serversSockets, [])
+    # print("writeable: ", writable)
+    # print("readable: ", readable)
+
+    # transfer response to clients
+    for read in readable:
+        if read is listeningSock:
+            # add new connection to list
+            connectionToClient, client_address = listeningSock.accept()
+            connectionToClient.setblocking(0)
+            clientsOpenedSockets.append(connectionToClient)
+        elif read in clientsOpenedSockets:
+            # recieve request and store it in queue
+            data = connectionToClient.recv(2)
+            if data:
+                requestsQueue.append((read, data))
+            # else:
+            #     if s in outputs:
+            #         outputs.remove(s)
+            #     inputs.remove(s)
+            #     s.close()
+            #     del message_queues[s]
+        else: #server socket
+            # send response to client
+            dataResponse = read.recv(2)
+            returnTo = handledConnections[read]
+            returnTo.send(dataResponse)
+
+            print("forward ", data, " from: ", returnTo.getpeername(), " to server: ", read.getpeername())
+
+            # notify that server is enable
+            isServerAvailable[read] = True
+            # remove connection and close it
+            clientsOpenedSockets.remove(returnTo)
+            returnTo.close()
 
     if (writable):
         # need to choose wisely the server to send to
-        connectionToServer = writable[0]
+        connectionToServer = peekServer(writable)
 
-        # send request to server
-        print ('sending "%s" to %s' % (data, connectionToServer.getpeername()))
-        # connectionToServer.setblocking(0)
-        connectionToServer.send(data)
+        #send meassage from queue
+        if ((connectionToServer is not None) and requestsQueue):
+            # pop next request from queue
+            connection, msg = requestsQueue[0]
+            requestsQueue.remove((connection, msg))
 
-        openConnections[connectionToServer] = connectionToClient
+            # handle request
+            handledConnections[connectionToServer] = connection
+            isServerAvailable[connectionToServer] = False
+            connectionToServer.send(msg)
 
-    # transfer response to clients
-    for readServer in readable:
-        dataResponse = readServer.recv(2)
-        returnTo = openConnections[readServer]
-        # returnTo.setblocking(0)
-        returnTo.send(dataResponse)
-
-        print ("forward ", data, " from: ", returnTo.getpeername(), " to server: ", readServer.getpeername())
-
-        returnTo.close()
+    # if (writable):
+    #
+    #     # need to choose wisely the server to send to
+    #     connectionToServer = p[0]
+    #
+    #     # send request to server
+    #     print ('sending "%s" to %s' % (data, connectionToServer.getpeername()))
+    #     # connectionToServer.setblocking(0)
+    #     connectionToServer.send(data)
+    #
+    #     handledConnections[connectionToServer] = connectionToClient
